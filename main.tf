e8e6df22931b27e104cd405dd93b7d847b86214a
Никита <Goldbergnik1993@gmail.com>
2026-03-28 12:27:34 +0100

    dev

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.8.4"
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "azuregoldbergnik1993"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
